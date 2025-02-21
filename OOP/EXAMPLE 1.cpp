2d9d85eab73dc926d4500e56989df3bde30e7494
uwera-creator <uwerajox@gmail.com>
2025-02-21 10:50:29 +0200

    Add files via upload

---
+++ OOP/EXAMPLE 1.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+ // Include input-output library
+using namespace std; 
+// Use standard namespace
+
+int main() {  
+// Main function
+    cout << "Hello, World!";  
+	// Print output to the screen
+    return 0;  
+	// Return 0 to indicate successful execution
+}
+
---
+++ OOP/EXAMPLE 4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 10, b = 3;
+    cout << "Sum: " << a + b << endl;           
+	// Outputs: Sum: 13
+    cout << "Difference: " << a - b << endl;   
+	 // Outputs: Difference: 7
+    cout << "Product: " << a * b << endl;       
+	// Outputs: Product: 30
+    cout << "Quotient: " << a / b << endl;    
+	  // Outputs: Quotient: 3
+    cout << "Remainder: " << a % b << endl;    
+	 // Outputs: Remainder: 1
+    return 0;
+}
+
